20b3e0f291ee427a0d452a8778c8c4ee936da2a4
Malavika <29malavika2005@gmail.com>
2026-04-12 17:06:46 +0530

    Corousel component and search input in home page are added

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
-
+import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+   items = ["assets/images/Burger.avif","assets/images/Cake.avif","assets/images/Dessert.avif",
+    "assets/images/Pizza.avif","assets/images/Rolls.avif","assets/images/NorthIndian.avif",
+  "assets/images/Tea.avif","assets/images/Chinease.avif"];
+
+}
